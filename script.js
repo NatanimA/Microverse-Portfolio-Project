138408010fc0fede8f3ebfe7312f39ed2b592ac9
natanimabesha <natanimabesha@www.zhewutech.com>
2022-09-05 16:24:45 +0300

    Fix js linters

--- script.js
+++ script.js
@@ -10,11 +10,9 @@ hamburger.addEventListener('click', () => {
   body.classList.toggle('active');
 });
 
-document.querySelectorAll('.mobile-nav-link').forEach((link) =>
-  link.addEventListener('click', () => {
-    headline.classList.remove('active');
-    hamburger.classList.remove('active');
-    navBar.classList.remove('active');
-    body.classList.remove('active');
-  })
-);
+document.querySelectorAll('.mobile-nav-link').forEach((link) => link.addEventListener('click', () => {
+  headline.classList.remove('active');
+  hamburger.classList.remove('active');
+  navBar.classList.remove('active');
+  body.classList.remove('active');
+}));
